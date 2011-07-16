ddff4fe914a54f50b8c71479d6a193bfa9034d8e
David Leatherman <david.leatherman@sonian.net>
2011-07-15 21:49:34 -0400

    Some more util functions

--- grc.el
+++ grc.el
@@ -109,10 +109,22 @@ color is added)"
       thing
     (list thing)))
 
+(defun grc-string (thing)
+  (if (stringp thing)
+      thing
+    (prin1-to-string thing t)))
+
 (defun grc-flatten (x)
   (cond ((null x) nil)
         ((listp x) (append (grc-flatten (car x)) (grc-flatten (cdr x))))
         (t (list x))))
+
+(defun grc-get-in (alist seq &optional not-found)
+  (let ((val (reduce (lambda (a k)
+                       (aget a k)) seq :initial-value alist)))
+    (if (and (null val) not-found)
+        not-found
+      val)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Google reader requests
